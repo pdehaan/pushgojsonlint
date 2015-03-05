a68582cbcec1dbe7ade2e9bb4270ce75590c6905
Peter deHaan <peter@deseloper.com>
2015-03-04 18:04:01 -0800

    minor resort of the schema's optional args

--- schema.js
+++ schema.js
@@ -10,21 +10,24 @@ module.exports = Joi.object().keys({
       'description': Joi.string().optional(),
       'steps': Joi.array().items(
         Joi.object().keys({
-          'additional_command_args': Joi.string().optional(),
+          // required
           'container_name': Joi.string(),
           'container_url': Joi.string(),
           'dns_name': Joi.string(),
           'docker_series': Joi.string(),
-          'environment_data': Joi.array(),
           'instance_count': Joi.number().integer(),
           'instance_region': Joi.string(),
           'instance_type': Joi.string(),
           'name': Joi.string(),
           'port_mapping': Joi.string(),
-          'prune_running': Joi.boolean().optional(),
-          'run_delay': Joi.number().optional(),
           'run_max_time': Joi.number().integer(),
-          'volume_mapping': Joi.string()
+          'volume_mapping': Joi.string(),
+
+          // optional
+          'additional_command_args': Joi.string().optional(),
+          'environment_data': Joi.array().optional(),
+          'prune_running': Joi.boolean().optional(),
+          'run_delay': Joi.number().optional()
         })
       )
     })
