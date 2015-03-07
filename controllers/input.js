3c0a301ba53faaac57c13b24316bd2c28b3dbe39
Peter deHaan <peter@deseloper.com>
2015-03-06 23:43:58 -0800

    Add default pushgo.json file to direct linting

--- controllers/input.js
+++ controllers/input.js
@@ -1,5 +1,21 @@
 'use strict';
 
+var stringify = require('canonical-json');
+
+var getp = require('../lib/utils').getp;
+
+var pushgojson = 'https://raw.githubusercontent.com/loads/loads-broker/master/pushgo.json';
+
 module.exports = function (request, reply) {
-  reply.view('direct');
+  getp(pushgojson).then(function (data) {
+    data = JSON.parse(data);
+    reply.view('direct', {
+      data: stringify(data, null, 2)
+    });
+  }, function (err) {
+    console.error(err);
+    reply.view('direct', {
+      data: ''
+    });
+  });
 };
